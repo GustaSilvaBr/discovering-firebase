bf21ea66e22407bde429dac7ba791dce258d126c
Gustavo Silva <gustavo.ferreira0821@gmail.com>
2022-04-21 16:54:28 -0300

    :sparkles: feat: show simple home page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
     return (
         <div className="App" >
-            Hello, world!
+            <h1>Discovering firebase</h1>
+            <h2>A project to learn about firebase features</h2>
+            <ol>
+                <li>Firebase project synchronized with git hub master branch</li>
+            </ol>
         </div>
     );
 }

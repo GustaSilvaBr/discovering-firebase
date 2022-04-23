4496ac29ead5414f2270224fc29ba99b9ed88286
Gustavo Silva <gustavo.ferreira0821@gmail.com>
2022-04-23 13:33:11 -0300

    :sparkles: feat: add message in firestore

--- functions/index.js
+++ functions/index.js
@@ -4,3 +4,14 @@ const functions = require("firebase-functions");
 //Accessing the firestore
 const admin = require('firebase-admin');
 admin.initializeApp();
+
+//Take the text and insert into Firestore
+//Under the path /messages/:documentId/original
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+    //Grab the text parameter
+    const original = req.query.text;
+    //Push the new message into Firestore by Firebase Admin SDK
+    const writeResult = await admin.firestore().collection('messages').add({ original: original });
+
+    res.json({ result: `Message with ID: ${writeResult.id} added.` });
+});

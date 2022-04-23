fcf7d26289832c48d8680f905066bc36b86cc3d4
Gustavo Silva <gustavo.ferreira0821@gmail.com>
2022-04-23 12:26:43 -0300

    :wrench: chore: Import modules and Init App

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,6 @@
+//Creating cloud functions and set up triggers
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//Accessing the firestore
+const admin = require('firebase-admin');
+admin.initializeApp();

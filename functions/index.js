182d94c70dc98c683421b70fe12f62e9b8811e92
Gustavo Silva <gustavo.ferreira0821@gmail.com>
2022-05-09 23:16:54 -0300

    :sparkles: feat: listen function

--- functions/index.js
+++ functions/index.js
@@ -9,9 +9,30 @@ admin.initializeApp();
 //Under the path /messages/:documentId/original
 exports.addMessage = functions.https.onRequest(async (req, res) => {
     //Grab the text parameter
-    const original = req.query.text;
+    const level = req.query.text;
     //Push the new message into Firestore by Firebase Admin SDK
-    const writeResult = await admin.firestore().collection('messages').add({ original: original });
+    const writeResult = await admin.firestore().collection('grades').add({ level: level });
 
-    res.json({ result: `Message with ID: ${writeResult.id} added.` });
+    return res.status(200).send({ result: `Message with ID: ${writeResult.id} added.` });
+});
+
+exports.makeUppercase = functions.firestore.document('/grades/{documentId}').onCreate((snap, context) => {
+    const level = snap.data().level;
+    let levelName = "";
+    switch (level) {
+        case '1':
+            levelName = "first"
+            break;
+        case '2':
+            levelName = "second"
+            break;
+        case '3':
+            levelName = "third"
+            break;
+        default:
+            levelName = "unknown"
+            break;
+    }
+
+    return snap.ref.set({ levelName: levelName }, { merge: true });
 });
